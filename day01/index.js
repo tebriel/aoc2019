957c214f5f780ea65e93736140d1e2adf8898363
Chris Moultrie <tebriel@github.com>
2019-12-04 15:42:13 -0500

    Handle day 1 part two

--- day01/index.js
+++ day01/index.js
@@ -25,18 +25,42 @@ function calculate_fuel(mass) {
 }
 
 /**
+ * Calculate the fuel required for the fuel
+ *
+ * @param {number} fuel - The current fuel requirement
+ * @returns {number} - The fuel required for the fuel input
+ */
+function calculate_fuel_for_fuel(fuel) {
+  let fuel_needed = calculate_fuel(fuel);
+  let result = fuel_needed;
+  while (fuel_needed > 0) {
+    fuel_needed = calculate_fuel(fuel_needed);
+    result += fuel_needed;
+  }
+  return result;
+}
+
+/**
  * Calculate the Fuel for a list of masses
  *
  * @param {Array} masses - The masses of the modules
- * @returns {number} - The total fuel required.
+ * @returns {object} - An object with the fuel required and the fuel for the fuel
  */
 function calculate_total(masses) {
-  return masses.reduce((accumulator, mass) => accumulator + calculate_fuel(mass), 0);
+  return masses.reduce((accumulator, mass) => {
+    const fuel = calculate_fuel(mass);
+    accumulator.fuel += fuel;
+    accumulator.fuelForFuel += calculate_fuel_for_fuel(fuel);
+    return accumulator;
+  }, { fuel: 0, fuelForFuel: 0 });
 }
 
-module.exports = { calculate_fuel, calculate_total };
+module.exports = { calculate_fuel, calculate_fuel_for_fuel, calculate_total };
 
 if (require.main === module) {
   const masses = fs.readFileSync(argv.data, 'utf-8').split('\n');
-  process.stdout.write(`${calculate_total(masses)}\n`);
+  const result = calculate_total(masses);
+  process.stdout.write(`Fuel For Mass: ${result.fuel}\n`);
+  process.stdout.write(`Fuel For Fuel: ${result.fuelForFuel}\n`);
+  process.stdout.write(`Total Fuel Required: ${result.fuel + result.fuelForFuel}\n`);
 }
--- day01/index.test.js
+++ day01/index.test.js
@@ -1,4 +1,4 @@
-const { calculate_fuel, calculate_total } = require('./index');
+const { calculate_fuel, calculate_fuel_for_fuel, calculate_total } = require('./index');
 
 describe('Tests the Fuel Calculation Module', () => {
   test.each([
@@ -14,8 +14,20 @@ describe('Tests the Fuel Calculation Module', () => {
   });
 
   test.each([
-    [[12, 14, 1969, 100756], 34241]
+    [[1969], 966],
+    [[100756], 50346],
+    [[12, 14, 1969, 100756], 51316]
   ])('.calculate_total(%j)', (masses, expected) => {
-    expect(calculate_total(masses)).toBe(expected);
+    const total = calculate_total(masses);
+    expect(total.fuel + total.fuelForFuel).toBe(expected);
+  });
+
+  test.each([
+    [2, 0],
+    [1969, 312],
+    [100756, 16763]
+  ])('.calculate_fuel_for_fuel(%i)', (mass, expected) => {
+    const fuel = calculate_fuel(mass);
+    expect(calculate_fuel_for_fuel(fuel)).toBe(expected);
   });
 });

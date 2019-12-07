bec3147138b040800aa02420a59466dc2df24338
Chris Moultrie <tebriel@github.com>
2019-12-07 13:09:58 -0500

    Update tests

--- day07/index.test.js
+++ day07/index.test.js
@@ -17,35 +17,35 @@ describe('Tests An Entire OpCode', () => {
     [
       [3, 9, 8, 9, 10, 9, 4, 9, 99, -1, 8],
       8,
-      { codes: [3, 9, 8, 9, 10, 9, 4, 9, 99, 1, 8], outputs: [1] }
+      1
     ],
     [
       [3, 9, 7, 9, 10, 9, 4, 9, 99, -1, 8],
       7,
-      { codes: [3, 9, 7, 9, 10, 9, 4, 9, 99, 1, 8], outputs: [1] }
+      1
     ],
     [
       [3, 3, 1108, -1, 8, 3, 4, 3, 99],
       8,
-      { codes: [3, 3, 1108, 1, 8, 3, 4, 3, 99], outputs: [1] }
+      1
     ],
     [
       [3, 3, 1107, -1, 8, 3, 4, 3, 99],
       7,
-      { codes: [3, 3, 1107, 1, 8, 3, 4, 3, 99], outputs: [1] }
+      1
     ],
     [
       [3, 12, 6, 12, 15, 1, 13, 14, 13, 4, 13, 99, -1, 0, 1, 9],
       0,
-      { codes: [3, 12, 6, 12, 15, 1, 13, 14, 13, 4, 13, 99, 0, 0, 1, 9], outputs: [0] }
+      0
     ],
     [
       [3, 3, 1105, -1, 9, 1101, 0, 0, 12, 4, 12, 99, 1],
       0,
-      { codes: [3, 3, 1105, 0, 9, 1101, 0, 0, 12, 4, 12, 99, 0], outputs: [0] }
+      0
     ]
   ])('.iterateOpCodes(%j, %i)', (codes, inputVal, expected) => {
-    expect(iterateOpCodes(codes, inputVal)).toStrictEqual(expected);
+    expect(iterateOpCodes(codes)(inputVal)).toStrictEqual(expected);
   });
 
   test.each([
@@ -56,6 +56,6 @@ describe('Tests An Entire OpCode', () => {
     const codes = [3, 21, 1008, 21, 8, 20, 1005, 20, 22, 107, 8, 21, 20, 1006, 20, 31,
       1106, 0, 36, 98, 0, 0, 1002, 21, 125, 20, 4, 20, 1105, 1, 46, 104,
       999, 1105, 1, 46, 1101, 1000, 1, 20, 4, 20, 1105, 1, 46, 98, 99];
-    expect(iterateOpCodes(codes, inputVal).outputs).toStrictEqual([expected]);
+    expect(iterateOpCodes(codes)(inputVal)).toStrictEqual(expected);
   });
 });
